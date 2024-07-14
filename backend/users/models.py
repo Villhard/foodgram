bb4c976bb0bf27277157a796f42756e95e6666ba
Victor Ryabov <victorwillhard@gmail.com>
2024-07-14 21:21:05 +0300

    feat: Add avatar field to CustomUser model

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     )
     first_name = models.CharField(max_length=150, blank=False, null=False)
     last_name = models.CharField(max_length=150, blank=False, null=False)
-    # TODO: Добавить аватар
+    avatar = models.ImageField(upload_to="media/", blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
