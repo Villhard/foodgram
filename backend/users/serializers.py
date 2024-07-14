fc88d3bf8f7da3110b1d5e230fefcbc9543a8a9f
Victor Ryabov <victorwillhard@gmail.com>
2024-07-14 21:21:17 +0300

    feat: Add avatar field to CustomUserSerializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
 
 User = get_user_model()
@@ -7,6 +8,7 @@ User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField()
+    avatar = Base64ImageField()
 
     # FIXME: Настроить логику определения подписки
     def get_is_subscribed(self, obj):
@@ -14,11 +16,11 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'username')
+        fields = ('id', 'email', 'first_name', 'last_name', 'username', 'avatar', 'is_subscribed')
 
 
 class CustomCreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'username', 'password')
+        fields = ('id', 'email', 'first_name', 'last_name', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
