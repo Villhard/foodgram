341c28afe6a59d6fc8f00975bff768bb9c53c2c2
Victor Ryabov <victorwillhard@gmail.com>
2024-07-20 21:48:39 +0300

    refactor: remove non-working serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from drf_extra_fields.fields import Base64ImageField
-from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient
-from users.serializers import UserSerializer
+from recipes.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -14,58 +12,3 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit')
-
-
-class RecipeIngredientSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(source='ingredient.id')
-    name = serializers.CharField(source='ingredient.name')
-    measurement_unit = serializers.CharField(
-        source='ingredient.measurement_unit'
-    )
-
-    class Meta:
-        model = RecipeIngredient
-        fields = ('id', 'name', 'measurement_unit', 'amount')
-
-
-class RecipeSerializer(serializers.ModelSerializer):
-    author = UserSerializer(read_only=True)
-    ingredients = RecipeIngredientSerializer(many=True, source='recipeingredient_set')
-    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all())
-    is_favorite = serializers.SerializerMethodField()
-    is_in_shopping_cart = serializers.SerializerMethodField()
-    image = Base64ImageField()
-
-    # FIXME
-    def get_is_favorite(self, obj):
-        return False
-    
-    # FIXME
-    def get_is_in_shopping_cart(self, obj):
-        return False
-
-    def create(self, validated_data):
-        ingredients = validated_data.pop('ingredients')
-        recipe = Recipe.objects.create(**validated_data)
-        for ingredient in ingredients:
-            RecipeIngredient.objects.create(
-                recipe=recipe,
-                ingredient=ingredient['ingredient'],
-                amount=ingredient['amount'],
-            )
-        return recipe
-
-    class Meta:
-        model = Recipe
-        fields = (
-            'id',
-            'tags',
-            'ingredients',
-            'author',
-            'name',
-            'image',
-            'text',
-            'cooking_time',
-            'is_favorite',
-            'is_in_shopping_cart',
-        )
