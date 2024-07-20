b0ef94336afac2547808344d33cf371140853319
Victor Ryabov <victorwillhard@gmail.com>
2024-07-20 18:12:20 +0300

    feat: add recipes' model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Tag(models.Model):
@@ -15,3 +19,25 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=256)
+    text = models.TextField()
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(1),)
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient, through='RecipeIngredient', related_name='recipes'
+    )
+    tags = models.ManyToManyField(Tag, related_name='recipes')
+    image = models.ImageField(upload_to='media/recipies/', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(1),)
+    )
