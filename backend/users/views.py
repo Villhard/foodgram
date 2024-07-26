074fe877e82e33018f5c0054a0f9529df3d284b8
Victor Ryabov <victorwillhard@gmail.com>
2024-07-26 21:45:19 +0300

    feat: add view for subscribes endpoints

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from djoser.views import UserViewSet as DjoserViewSet
-from users.serializers import AvatarSerializer
+from users.serializers import AvatarSerializer, ExtendedCustomUserSerializer
+from users.models import Subscription
 
 User = get_user_model()
 
@@ -40,3 +41,48 @@ class UserViewSet(DjoserViewSet):
             user.avatar = None
             user.save()
             return Response(status=204)
+
+    @action(
+        methods=('get',),
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+        serializer_class=ExtendedCustomUserSerializer,
+        url_path='me/subscriptions',
+    )
+    def subscriptions(self, request):
+        user = request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
+    @action(
+        methods=('post', 'delete'),
+        detail=True,
+        permission_classes=(IsAuthenticated,),
+        serializer_class=ExtendedCustomUserSerializer,
+        url_path='subscribe',
+    )
+    def subscribe(self, request, id=None):
+        user = request.user
+        author = self.get_object()
+
+        if request.method == 'POST':
+            if not user.following.filter(id=author.id).exists():
+                subcsription = Subscription.objects.create(
+                    follower=user, following=author
+                )
+                serializer = self.get_serializer(author)
+                return Response(serializer.data, status=201)
+            else:
+                return Response(
+                    {'detail': 'Вы уже подписаны на этого автора'},
+                    status=400
+                )
+        elif request.method == 'DELETE':
+            if not user.following.filter(id=author.id).exists():
+                return Response(
+                    {'detail': 'Вы не подписаны на этого автора'},
+                    status=400
+                )
+            else:
+                user.following.get(id=author.id).delete()
+                return Response(status=204)
