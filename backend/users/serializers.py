7fc515d650c30f40495ddda95c07672d822a2304
Victor Ryabov <victorwillhard@gmail.com>
2024-07-26 21:44:38 +0300

    feat: make extended user's class for subscribe info

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -45,3 +45,25 @@ class CustomCreateUserSerializer(UserCreateSerializer):
 
 class AvatarSerializer(serializers.Serializer):
     avatar = Base64ImageField()
+
+
+class ExtendedCustomUserSerializer(CustomUserSerializer):
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+
+    def get_recipes(self, obj):
+        from api.serializers import RecipeSerializer
+        recipes = obj.recipes.all()
+        serializer = RecipeSerializer(recipes, many=True, context=self.context)
+        return serializer.data
+
+    @staticmethod
+    def get_recipes_count(obj):
+        return obj.recipes.count()
+
+    class Meta:
+        model = User
+        fields = CustomUserSerializer.Meta.fields + (
+            'recipes',
+            'recipes_count',
+        )
