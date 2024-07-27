150d2b423aa445405c456e2bda6da99cfc1c9672
Victor Ryabov <victorwillhard@gmail.com>
2024-07-27 13:56:33 +0300

    feat: add endpoints for shopping_cart and implement download shopping_cart

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
+from io import StringIO
 from rest_framework import viewsets, mixins
+from django.db import models
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from django.db.utils import IntegrityError
@@ -11,8 +13,9 @@ from api.serializers import (
     RecipeSerializer,
     ShortRecipeSerializer,
 )
-from recipes.models import Tag, Ingredient, Recipe
+from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient
 from favorites.models import Favorite
+from shopping.models import ShoppingCart
 from api.permissions import IsAuthorOrReadOnly
 
 
@@ -74,3 +77,64 @@ class RecipeViewSet(viewsets.ModelViewSet):
                     {'detail': 'Рецепт не найден в избранном'},
                     status=400,
                 )
+
+    # FIXME: refactor this method
+    @action(
+        detail=True,
+        methods=('post', 'delete'),
+        url_path='shopping_cart',
+        permission_classes=(IsAuthenticated,),
+    )
+    def shopping_cart(self, request, pk):
+        if request.method == 'POST':
+            user = request.user
+            recipe = self.get_object()
+            try:
+                ShoppingCart.objects.create(user=user, recipe=recipe)
+                serializer = ShortRecipeSerializer(recipe)
+                return Response(serializer.data, status=201)
+            except IntegrityError:
+                return Response(
+                    {'detail': 'Рецепт уже добавлен в список покупок'},
+                    status=400,
+                )
+        elif request.method == 'DELETE':
+            user = request.user
+            recipe = self.get_object()
+            try:
+                favorite = ShoppingCart.objects.get(user=user, recipe=recipe)
+                favorite.delete()
+                return Response(status=204)
+            except ShoppingCart.DoesNotExist:
+                return Response(
+                    {'detail': 'Рецепт не найден в списке покупок'},
+                    status=400,
+                )
+
+    @action(
+        detail=False,
+        methods=('get',),
+        url_path='download_shopping_cart',
+        permission_classes=(IsAuthenticated,),
+    )
+    def download_shopping_cart(self, request):
+        ingredients = RecipeIngredient.objects.filter(
+            recipe__in_cart__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit',
+        ).annotate(
+            total_amount=models.Sum('amount')
+        )
+        output = StringIO()
+        output.write('Список покупок:\n')
+        for ingredient in ingredients:
+            output.write(
+                f'{ingredient["ingredient__name"]} - {ingredient["total_amount"]} '
+                f'{ingredient["ingredient__measurement_unit"]}\n'
+            )
+        response = Response(output.getvalue(), content_type='text/plain')
+        response['Content-Disposition'] = (
+            'attachment; filename="shopping_cart.txt"'
+        )
+        return response
