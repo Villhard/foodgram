299abebaf75acf0fafbc41ecf66f4feae211b691
Victor Ryabov <victorwillhard@gmail.com>
2024-07-28 12:34:03 +0300

    chore: set up admin-zone

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, Tag, Recipe, RecipeIngredient
 
 
-admin.site.register(Ingredient)
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientInline,)
+    search_fields = ('name', 'author')
+    list_filter = ('tags',)
+    list_display = ('name', 'author', 'favorite_count')
+
+    @staticmethod
+    def favorite_count(obj):
+        return obj.favorites.count()
+
+    favorite_count.short_description = 'В избранном'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
 admin.site.register(Tag)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from users.models import Subscription
 
 
 User = get_user_model()
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username')
+    search_fields = ('email', 'username')
+    readonly_fields = ('date_joined', 'last_login')
+
+
+admin.site.register(Subscription)
