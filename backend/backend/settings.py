68336229db9a042d4424be31b695c35c479da8b1
Victor Ryabov <victorwillhard@gmail.com>
2024-07-30 21:16:46 +0300

    feat: set up docker compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,7 @@ SECRET_KEY = (
 
 DEBUG = True
 
-HOST = 'http://localhost:8000'
+HOST = 'http://localhost'
 
 ALLOWED_HOSTS = []
 
@@ -95,11 +96,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', '')
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -129,5 +133,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
+cp -r /collected_static/ /backend_static/
 
 gunicorn --bind :8000 backend.wsgi
