4855baaae44662c4bc7f6dbbfc0229be039ae2fa
Victor Ryabov <victorwillhard@gmail.com>
2024-08-03 23:05:49 +0300

    fix(user/api): the logic has been moved from the users application to the api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,84 @@
-from rest_framework import serializers
-from drf_extra_fields.fields import Base64ImageField
 from recipes.models import Tag, Ingredient, RecipeIngredient, Recipe
-from users.serializers import CustomUserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from drf_extra_fields.fields import Base64ImageField
+
+User = get_user_model()
+
+
+class CustomUserSerializer(UserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+    avatar = Base64ImageField()
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_anonymous:
+            return False
+        return obj.following.filter(follower=user).exists()
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'username',
+            'avatar',
+            'is_subscribed',
+        )
+
+
+class CustomCreateUserSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'username',
+            'password',
+        )
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class AvatarSerializer(serializers.Serializer):
+    avatar = Base64ImageField()
+
+
+class ExtendedCustomUserSerializer(CustomUserSerializer):
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_recipes(obj):
+        recipes = obj.recipes.all()
+        serializer = ShortRecipeSerializer(recipes, many=True)
+        return serializer.data
+
+    @staticmethod
+    def get_recipes_count(obj):
+        return obj.recipes.count()
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        recipes_limit = request.query_params.get('recipes_limit')
+        recipes = instance.recipes.all()
+        if recipes_limit:
+            recipes = recipes[: int(recipes_limit)]
+        user_data = CustomUserSerializer(instance, context=self.context).data
+        user_data['recipes'] = ShortRecipeSerializer(recipes, many=True).data
+        user_data['recipes_count'] = instance.recipes.count()
+        return user_data
+
+    class Meta:
+        model = User
+        fields = CustomUserSerializer.Meta.fields + (
+            'recipes',
+            'recipes_count',
+        )
 
 
 class TagSerializer(serializers.ModelSerializer):
