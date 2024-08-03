c1b3c489ea63e1882afcd3378d6cb4f8c11b24db
Victor Ryabov <victorwillhard@gmail.com>
2024-08-04 01:16:19 +0300

    fix(filters): RecipeFilter with additional filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,15 +1,20 @@
+from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
+
+User = get_user_model()
 
 
 class RecipeFilter(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
     )
     author = filters.ModelChoiceFilter(
         field_name='author',
-        lookup_expr='in',
+        queryset=User.objects.all(),
     )
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter',
