bf2e37dca7a68f80d79225dedc6c6b2df33d594e
Victor Ryabov <victorwillhard@gmail.com>
2024-08-04 14:23:49 +0300

    fix(views): remove self parameter from get_recipe_by_short_link function

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -7,7 +7,7 @@ from recipes.models import Recipe
 
 
 @api_view(['GET'])
-def get_recipe_by_short_link(self, request, link):
+def get_recipe_by_short_link(request, link):
     recipe_id = Base52.from_base52(link)
     get_object_or_404(Recipe, id=recipe_id)
     return redirect(f'{HOST}/recipes/{recipe_id}')
